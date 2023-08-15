7f0eeb4fe39012eb4520893114c6430235b4abf9
AvniKahraman <avnikahraman79@gmail.com>
2023-08-15 19:52:38 +0300

    I have done the layout part of the drawer menu of Notepad, I will do the java part tomorrow
    I have done the layout part of the drawer menu of Notepad, I will do the java part tomorrow

---
+++ app/src/main/java/com/example/notpad/NoteAddFragment.java
@@ -0,0 +1,21 @@
+package com.example.notpad;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NoteAddFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_add, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/notpad/NoteEditFragment.java
@@ -0,0 +1,20 @@
+package com.example.notpad;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NoteEditFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_edit, container, false);
+    }
+}
